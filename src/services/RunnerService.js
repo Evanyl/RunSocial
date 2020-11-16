2d0a7c5a40df1ee5aa17ef6c5c5ad6c622afb0d6
Evan <liuyievan@gmail.com>
2020-11-16 00:52:50 -0800

    Fixed cors

--- src/services/RunnerService.js
+++ src/services/RunnerService.js
@@ -1,22 +1,24 @@
 import axios from 'axios';
+import https from 'https';
 
 class RunnerService {
-    loginKey = "";
-
-    setLoginKey(loginKey) {
-        this.loginKey = loginKey;
-    }
 
     getApiUrl() {
-        return "https://ec2-54-173-26-187.compute-1.amazonaws.com:8080/api/v1/runner";
+        return "https://ec2-54-173-26-187.compute-1.amazonaws.com/api/v1/runner";
+    }
+
+    agent() {
+        return new https.Agent({  
+            rejectUnauthorized: false
+          });
     }
 
     checkValidKey() {
-        return axios.get(this.getApiUrl() + "/" + localStorage.getItem("loginKey"));
+        return axios.get(this.getApiUrl() + "/" + localStorage.getItem("loginKey"), { httpsAgent: this.agent() });
     }
 
     createRunner(runner) {
-        return axios.post(this.getApiUrl(), runner);
+        return axios.post(this.getApiUrl(), runner, { httpsAgent: this.agent() });
     }
 }
 export default new RunnerService();
--- src/services/WorkoutService.js
+++ src/services/WorkoutService.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
+import https from 'https';
 
 class WorkoutService {
-    loginKey = ""
-
-    setLoginKey(loginKey) {
-        this.loginKey = loginKey;
-    }
-
     checkEmpty() {
         if (localStorage.getItem("loginKey") === "") {
             return true;
@@ -14,28 +9,34 @@ class WorkoutService {
         return false;
     }
 
+    agent() {
+        return new https.Agent({  
+            rejectUnauthorized: false
+          });
+    }
+
     getApiUrl() {
-        return "https://ec2-54-173-26-187.compute-1.amazonaws.com:8080/api/v1/" + localStorage.getItem("loginKey") + "/workouts"
+        return "https://ec2-54-173-26-187.compute-1.amazonaws.com:443/api/v1/" + localStorage.getItem("loginKey") + "/workouts"
     }
 
     getWorkouts() {
-        return axios.get(this.getApiUrl());
+        return axios.get(this.getApiUrl(), {httpsAgent: this.agent()});
     }
 
     createWorkout(workout) {
-        return axios.post(this.getApiUrl(), workout);
+        return axios.post(this.getApiUrl(), workout, { httpsAgent: this.agent() });
     }
 
     getWorkoutById(id) {
-        return axios.get(this.getApiUrl() + '/' + id);
+        return axios.get(this.getApiUrl() + '/' + id, { httpsAgent: this.agent() });
     }
 
     updateWorkout(workout, id) {
-        return axios.put(this.getApiUrl() + '/' + id, workout);
+        return axios.put(this.getApiUrl() + '/' + id, workout, { httpsAgent: this.agent() });
     }
 
     deleteWorkout(id) {
-        return axios.delete(this.getApiUrl() + '/' + id);
+        return axios.delete(this.getApiUrl() + '/' + id, { httpsAgent: this.agent() });
     }
 }
 
